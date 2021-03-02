e206fd0528b7f7fea87d3df89caf01686507af7c
amaan-imran <78930620+amaan-imran@users.noreply.github.com>
2021-03-02 09:58:42 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 
-let  firstname = 'amaan';
+function Welcome(){
+    let username = prompt("What is your name?");
+    let message="Welcome " + username;
+    document.getElementById('saywelcome').innerHTML = message;  
 
-console.log(firstname);
+}
