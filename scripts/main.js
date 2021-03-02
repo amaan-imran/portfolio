315a9d6b4437ca11dc5511624db0c4115663ecac
amaan-imran <78930620+amaan-imran@users.noreply.github.com>
2021-03-02 11:03:35 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,20 @@ function Welcome(){
     document.getElementById('saywelcome').innerHTML = message;  
 
 }
+function lights(){
+
+     let color= prompt("What color is the light now?");
+    if (color === "red") {
+        console.log("STOP!");
+    }
+    else if (color=== "green"){
+        console.log("GO!");
+    }
+    else if (color ==="amber"){
+        console.log("Stop if it is safe to do so");
+    }
+    else {
+        console.log("invalid input");
+    }
+}lights();
+
