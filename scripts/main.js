948305276f4f1caceb129fe6ceb2a31cb611ffaf
amaan-imran <78930620+amaan-imran@users.noreply.github.com>
2021-03-07 22:15:27 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,10 @@
-
+// Greetings function
 function Welcome(){
     let username = prompt("What is your name?");
     let message="Welcome " + username;
     document.getElementById('saywelcome').innerHTML = message;  
 }
+// Lights
 function lights(){
 
      let color= prompt("This website will ask you question, are you ok with that?");
@@ -20,23 +21,25 @@ function lights(){
         console.log("invalid input");
     }
 }
-// lights()
-
+// Pizza Question
 function Pizza(){
     let topping = prompt("What is your favourite Pizza topping");
     let message= topping + " Pizza";
     document.getElementById('pizza').innerHTML = message;  
 }
+// Song question
 function Song(){
     let songtitle = prompt("What is your favourite song?");
     let message= songtitle;
     document.getElementById('favsong').innerHTML = message;  
 }
+// Movie Question
 function Movie(){
     let moviename = prompt("What is your favourite song?");
     let message= moviename + " -Is a good choice!";
     document.getElementById('favmovie').innerHTML = message;  
 }
+// Sport Question
 function Sport(){
     let sportname = prompt("What is your favourite song?");
     let message= sportname;
