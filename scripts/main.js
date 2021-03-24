291a820f2b9d2a5cc4bb2a5f3a59d7045637a274
amaan-imran <78930620+amaan-imran@users.noreply.github.com>
2021-03-24 15:32:04 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -150,23 +150,114 @@ function whileSumThrees() {
 			}
 		x++;
 		}
-	console.log(sum1)
+        document.getElementById("whilesumthree").innerHTML=sum1;
 }
 
-// Create a JavaScript function, tenFour() that iterates (counts through) the integers from 1 to 100. 
-// For multiples of 4 print the word Four instead of the number and for the multiples of 10 print Ten. 
-// For numbers which are multiples of both four and ten print TenFour!.
+ //Reiterates integers fron 1-100 (multiples of 4 = FOUR , multiples of 10 = TEN, multiples of 4&10 = TenFour!)
+//logs all output to console. The way this code runs similar to the for loop forsumofthrees().
 function tenFour() {
-	for (var i = 1; i <= 100; i++) {
-	 		if (i % 4 === 0 && i % 10 === 0) {
-	 				console.log(i + "TenFour!");
-	 		}	else if (i % 4 === 0) {
-	 				console.log(i + "Four");
-	 		}	else if (i % 10 === 0) {
-	 				console.log(i + "Ten");
-	 		}	else {
-	 				console.log(i);
-	 		}
-	 } 
+    for (i = 1; i <= 100; i++) {
+        if (i % 4 === 0 && i % 10 === 0) {
+            console.log("TenFour!");
+        }
+        else if (i % 4 === 0) {
+            console.log("FOUR");
+        }
+        else if (i % 10 === 0) {
+            console.log("TEN");
+        }
+        else {
+            console.log(i);
+        }
+    }
 }
 
+
+//Finds what century it is with a given year
+function centuryFromYear() {
+    var x = parseInt(prompt("Enter a year: "));
+    if (x >= 0 && x <= 100) {
+        output = 1;
+    }
+    if (x >= 101 && x <= 200) {
+        output = 2;
+    }
+    if (x >= 201 && x <= 300) {
+        output = 3;
+    }
+    if (x >= 301 && x <= 400) {
+        output = 4;
+    }
+    if (x >= 401 && x <= 500) {
+        output = 5;
+    }
+    if (x >= 501 && x <= 600) {
+        output = 6;
+    }
+    if (x >= 601 && x <= 700) {
+        output = 7;
+    }
+    if (x >= 701 && x <= 800) {
+        output = 8;
+    }
+    if (x >= 801 && x <= 900) {
+        output = 9;
+    }
+    if (x >= 901 && x <= 1000) {
+        output = 10;
+    }
+    if (x >= 1001 && x <= 1100) {
+        output = 11;
+    }
+    if (x >= 1101 && x <= 1200) {
+        output = 12;
+    }
+    if (x >= 1201 && x <= 1300) {
+        output = 13;
+    }
+    if (x >= 1301 && x <= 1400) {
+        output = 14;
+    }
+    if (x >= 1401 && x <= 1500) {
+        output = 15;
+    }
+    if (x >= 1501 && x <= 1600) {
+        output = 16;
+    }
+    if (x >= 1601 && x <= 1700) {
+        output = 17;
+    }
+    if (x >= 1701 && x <= 1800) {
+        output = 18;
+    }
+    if (x >= 1801 && x <= 1900) {
+        output = 19;
+    }
+    if (x >= 1901 && x <= 2000) {
+        output = 20;
+    }
+    if (x >= 2001 && x <= 2100) {
+        output = 21;
+    }
+document.getElementById("century").innerHTML = output;
+} 
+
+//Finds the third angle with two angles of a triangle
+function thirdAngle() {
+    var angle1 = parseInt(prompt("Enter Angle one: "));
+    var angle2 = parseInt(prompt("Enter Angle two: "));
+    var angletotal = 180 - angle1 - angle2;
+    if (angletotal <= 180) {
+       output = angletotal;
+    }
+    if (angle1 < 0) {
+        output = "Invalid. Enter two angles of a triangle";
+    }
+    if (angle2 < 0) {
+        output = "Invalid. Enter two angles of a triangle";
+    }
+    if (angletotal < 0) {
+        output = "Invalid. Enter two angles of a triangle";
+    }
+document.getElementById("angles").innerHTML = output;
+}
